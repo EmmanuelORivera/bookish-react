901e39247fd8fcd652affc19c47eb9d2f60c1de9
Emmanuel Ochoa <eorivera95@gmail.com>
2021-08-30 17:56:29 -0600

    testing fetch books by mocking the store and the axios.get function

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,1 +1,16 @@
+import axios from "axios";
+
+export const fetchBooks = (term = "") => {
+  return (dispatch) => {
+    dispatch({ type: "FETCH_BOOKS_PENDING" });
+    return axios
+      .get(`http://localhost:8080/books?q=${term}`)
+      .then((res) => {
+        dispatch({ type: "FETCH_BOOKS_SUCCESS", books: res.data });
+      })
+      .catch((err) => {
+        dispatch({ type: "FETCH_BOOKS_FAILED", err: err.message });
+      });
+  };
+};
 export const setSearchTerm = (term) => ({ type: "SET_SEARCH_TERM", term });
--- src/redux/actions/actions.test.js
+++ src/redux/actions/actions.test.js
@@ -1,14 +1,80 @@
-import { setSearchTerm } from "./actions";
+import { fetchBooks, setSearchTerm } from "./actions";
+import axios from "axios";
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import {
+  FETCH_BOOKS_FAILED,
+  FETCH_BOOKS_PENDING,
+  FETCH_BOOKS_SUCCESS,
+  SET_SEARCH_TERM,
+} from "../types";
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe("BookListContainer related actions", () => {
   it("Sets the search keyword", () => {
     const term = "";
     const expected = {
-      type: "SET_SEARCH_TERM",
+      type: SET_SEARCH_TERM,
       term,
     };
 
     const action = setSearchTerm(term);
     expect(action).toEqual(expected);
+  });
+
+  it("Fetches data successfully", () => {
+    const books = [
+      { id: 1, name: "Refactoring" },
+      { id: 2, name: "Domain-driven design" },
+    ];
+
+    axios.get = jest.fn(() => Promise.resolve({ data: books }));
+
+    const expectedActions = [
+      { type: FETCH_BOOKS_PENDING },
+      { type: FETCH_BOOKS_SUCCESS, books },
+    ];
+
+    const store = mockStore({ books: [] });
+
+    return store.dispatch(fetchBooks()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
+  it("Fetch data with error", () => {
+    axios.get = jest.fn(() =>
+      Promise.reject({ message: "Something went wrong" })
+    );
+
+    const expectedActions = [
+      { type: FETCH_BOOKS_PENDING },
+      { type: FETCH_BOOKS_FAILED, err: "Something went wrong" },
+    ];
+
+    const store = mockStore({ books: [] });
+
+    return store.dispatch(fetchBooks()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
+  it("Search data with term", () => {
+    const books = [
+      { id: 1, name: "Refactoring" },
+      { id: 2, name: "Domain-driven design" },
+    ];
+
+    axios.get = jest.fn(() => Promise.resolve({ data: books }));
+
+    const store = mockStore({ books: [] });
+
+    return store.dispatch(fetchBooks("domain")).then(() => {
+      expect(axios.get).toHaveBeenCalledWith(
+        "http://localhost:8080/books?q=domain"
+      );
+    });
   });
 });
