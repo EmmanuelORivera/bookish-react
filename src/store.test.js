6de1c7fc38ce1709e50e8fab445fba8a2d98538e
Emmanuel Ochoa <eorivera95@gmail.com>
2021-08-31 21:05:37 -0600

    testing a search with the store

--- src/store.test.js
+++ src/store.test.js
@@ -4,6 +4,7 @@ import store from "./store";
 
 describe("Store", () => {
   const books = [{ id: 1, name: "Refactoring" }];
+  const term = "domain";
   it("Fetch books from remote", () => {
     axios.get = jest.fn(() => Promise.resolve({ data: books }));
 
@@ -11,6 +12,20 @@ describe("Store", () => {
       const state = store.getState();
       expect(state.books.length).toEqual(1);
       expect(state.books).toEqual(books);
+    });
+  });
+
+  it("Perform a search", () => {
+    axios.get = jest.fn(() => Promise.resolve({ data: books }));
+    store.dispatch(actions.setSearchTerm(term));
+
+    return store.dispatch(actions.fetchBooks()).then(() => {
+      const state = store.getState();
+
+      expect(state.term).toEqual(term);
+      expect(axios.get).toHaveBeenCalledWith(
+        `http://localhost:8080/books?q=${term}`
+      );
     });
   });
 });
